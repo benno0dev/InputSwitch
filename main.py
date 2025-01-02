c17f10448979df26f19164fce2e2a7c5126e55a5
benno0dev <benno.doehr@gmx.de>
2025-01-02 23:00:46 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from pystray import Icon, Menu, MenuItem
+from PIL import Image, ImageDraw
+import monitorcontrol as mc
+
+def create_image(): # Draws the Icon
+    width = 64
+    height = 64
+    image = Image.new('RGBA', (width, height), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(image)
+
+    draw.ellipse((16, 16, 48, 48), fill='green')
+
+    return image
+
+Monitor = 1 # Change the Int to change the disired monitor (0 is probably always the main monitor)
+
+def on_hdmi1():
+    monitors = mc.get_monitors()
+    with monitors[Monitor]:
+        monitors[Monitor].set_input_source("HDMI1")
+        print("Monitor set to HDMI 1")
+
+def on_hdmi2():
+    monitors = mc.get_monitors()
+    with monitors[Monitor]:
+        monitors[Monitor].set_input_source("HDMI2")
+        print("Monitor set to HDMI 2")
+
+def on_dp():
+    monitors = mc.get_monitors()
+    with monitors[Monitor]:
+        monitors[Monitor].set_input_source("DP1")
+        print("Monitor set to DisplayPort")
+        
+
+def on_quit(icon, item):
+    icon.stop()
+
+# Define the menu
+menu = Menu(
+    MenuItem('HDMI 1', lambda: on_hdmi1()),
+    MenuItem('HDMI 2', lambda: on_hdmi2()),
+    MenuItem('DisplayPort', lambda: on_dp()),
+    MenuItem('Quit', on_quit)
+)
+
+icon = Icon("InputSwitch", create_image(), "InputSwitch Tray Icon", menu)
+
+icon.run()
